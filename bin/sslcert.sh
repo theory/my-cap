414582c0bd7f1300997ac51606bbe96fcaabee30
David Wheeler <david@justatheory.com>
2008-03-21 19:18:58 +0000

    Commented-out client cert stuff. I'm not using it, and likely won't be using it.

--- bin/sslcert.sh
+++ bin/sslcert.sh
@@ -19,15 +19,15 @@ openssl ca -batch -out servercert.pem.tmp -config openssl.cnf -infiles serverreq
 # Strip out the human-readable portion.
 openssl x509 -in servercert.pem.tmp -out servercert.pem
 
-# Create a client key and certificate request.
-perl -i -pe 's/^nsCertType = server/nsCertType = client/' openssl.cnf
-openssl req -new -nodes -keyout clientkey.pem -out clientreq.pem -config openssl.cnf -days 3650
-
-# Sign the certificate.
-openssl ca -batch -out clientcert.pem.tmp -config openssl.cnf -infiles clientreq.pem
-
-# Strip out the human-readable portion.
-openssl x509 -in clientcert.pem.tmp -out clientcert.pem
+# # Create a client key and certificate request.
+# perl -i -pe 's/^nsCertType = server/nsCertType = client/' openssl.cnf
+# openssl req -new -nodes -keyout clientkey.pem -out clientreq.pem -config openssl.cnf -days 3650
+# 
+# # Sign the certificate.
+# openssl ca -batch -out clientcert.pem.tmp -config openssl.cnf -infiles clientreq.pem
+# 
+# # Strip out the human-readable portion.
+# openssl x509 -in clientcert.pem.tmp -out clientcert.pem
 
 # Make sure that the SSL directories exist.
 # XXX Modify for other OSs. This is for Ubuntu.
@@ -41,19 +41,19 @@ cat serverkey.pem servercert.pem > /etc/ssl/private/serverkeycert.pem # Combined
 mv cacert.pem /etc/ssl/certs        # CA certifcate
 mv servercert.pem /etc/ssl/certs    # wildcard server certificate
 mv serverkey.pem /etc/ssl/private   # private key for wildcard certficate
-mv clientcert.pem /etc/ssl/certs    # client certificate
-mv clientkey.pem /etc/ssl/private   # private key for client certficate
+# mv clientcert.pem /etc/ssl/certs    # client certificate
+# mv clientkey.pem /etc/ssl/private   # private key for client certficate
 
 # Set their permissions.
 chmod 444 /etc/ssl/certs/cacert.pem
 chmod 444 /etc/ssl/certs/servercert.pem
 chmod 440 /etc/ssl/private/serverkey.pem
 chmod 440 /etc/ssl/private/serverkeycert.pem
-chmod 444 /etc/ssl/certs/clientcert.pem
-chmod 440 /etc/ssl/private/clientkey.pem
+# chmod 444 /etc/ssl/certs/clientcert.pem
+# chmod 440 /etc/ssl/private/clientkey.pem
 chgrp ssl-cert /etc/ssl/private/serverkey.pem
 chgrp ssl-cert /etc/ssl/private/serverkeycert.pem
-chgrp ssl-cert /etc/ssl/private/clientkey.pem
+# chgrp ssl-cert /etc/ssl/private/clientkey.pem
 
 # Delete the directory used to create them.
 cd ..
