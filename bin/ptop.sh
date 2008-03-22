1e0ad7cc4f5b5db6dd5cbab282a5f705e5277d0b
David Wheeler <david@justatheory.com>
2008-03-22 22:42:58 +0000

    New version of ptop. Also fixed unpacking bug.

--- bin/ptop.sh
+++ bin/ptop.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-export VERSION=3.6.2-beta1
+export VERSION=3.6.2-beta2
 
 . `dirname $0`/functions.sh
 
-setup /usr/local/include/expat.h
+# setup /usr/local/include/expat.h
 download http://pgfoundry.org/frs/download.php/1694/ptop-$VERSION.tar.bz2
 if [ $OS = 'Darwin' ]; then
+    tar jxf ptop-$VERSION.tar.bz2
     cd ptop-$VERSION
     ./configure --with-module=macosx
     make
