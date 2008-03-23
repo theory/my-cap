53abdf0fc5bb3f4f8c32e059493996ad69e80270
David Wheeler <david@justatheory.com>
2008-03-23 02:19:11 +0000

    * Restored courier-imap-ssl, as it's required for TLS. I figured out how to properly disable the imaps port, though, so we're good.  * Added `sys:adduser` and `sys:deluser` as ways to add and remove users, including LDAP records (and therefore email -- w00t!).  * Added a `Maildir` directory to `/etc/skel` by calling `maildirmake`.

--- lib/util.rb
+++ lib/util.rb
@@ -36,3 +36,59 @@ def sudol_scripts( *scripts )
     sudol "#{ BASE }/bin/#{ script }.sh"
   end
 end
+
+def run_ldap( cmd )
+  run cmd do |ch, stream, data|
+    if data =~ /^Please enter your password:/
+      ch.send_data( Capistrano::CLI.password_prompt + "\n" )
+    end
+  end
+end
+
+def user_ldif( user, pass, uid, gid )
+  require 'base64'
+  self[:last] ||= user.capitalize
+  ldif = %{dn: uid=#{ user },ou=people,dc=kineticode,dc=com
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: inetLocalMailRecipient
+uid: #{ user }
+mail: #{ user }@kineticode.com
+mailLocalAddress: #{ user }@kineticode.com\n}
+  [
+    [ :email,  'mail' ],
+    [ :email,  'mailLocalAddress'         ],
+    [ :name,   'cn'                       ],
+    [ :last,   'sn'                       ],
+    [ :first,  'givenName'                ],
+    [ :init,   'initials'                 ],
+    [ :title,  'title'                    ],
+    [ :work,   'telephoneNumber'          ],
+    [ :home,   'homeTelephoneNumber'      ],
+    [ :mobile, 'mobile'                   ],
+    [ :fax,    'facsimileTelephoneNumber' ],
+    [ :url,    'labeledURI'               ],
+  ].each do | var, label |
+    if val = self[var]
+      ldif += "#{ label }: #{ val }\n"
+    end
+  end
+
+  ldif += %{mailHost: mail.kineticode.com
+loginShell: /bin/bash
+uidNumber: #{ uid }
+gidNumber: #{ gid }
+homeDirectory: /home/#{ user }
+gecos: #{ self[:name] }
+preferredLanguage: en-us
+mailRoutingAddress: #{ user }@kineticode.com
+userPassword:: #{ Base64.encode64( pass )}\n}
+
+  return ldif
+end
+
+def genpass
+  chars = (33 .. 126).map { |i| i.chr }
+  size  = (8..16).to_a[ rand(9) ]
+  ( 1 .. size ).map { |a| chars[ rand( chars.size ) ] }.join
+end
