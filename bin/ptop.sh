0970991f0e6e642ce381ea9d7600a82a64d4a129
David Wheeler <david@justatheory.com>
2008-04-01 19:56:13 +0000

    Simpler build with the latest beta.

--- bin/ptop.sh
+++ bin/ptop.sh
@@ -4,15 +4,6 @@ export VERSION=3.6.2-beta2
 
 . `dirname $0`/functions.sh
 
-# setup /usr/local/include/expat.h
-download http://pgfoundry.org/frs/download.php/1694/ptop-$VERSION.tar.bz2
-if [ $OS = 'Darwin' ]; then
-    tar jxf ptop-$VERSION.tar.bz2
-    cd ptop-$VERSION
-    ./configure --with-module=macosx
-    make
-    make install
-    cd ..
-else
-    build ptop-$VERSION
-fi
+setup /usr/local/bin/ptop
+download http://pgfoundry.org/frs/download.php/1718/ptop-$VERSION.tar.bz2
+build ptop-$VERSION
