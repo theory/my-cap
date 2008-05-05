b565811736d342774309d61d4b303a3a283cabd1
David Wheeler <david@justatheory.com>
2008-05-05 17:23:59 +0000

    New version of pg_top.

--- bin/pg_top.sh
+++ bin/pg_top.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export VERSION=3.6.2-beta4
+export VERSION=3.6.2
 
 . `dirname $0`/functions.sh
 
 setup /usr/local/bin/pg_top
-download http://pgfoundry.org/frs/download.php/1756/pg_top-$VERSION.tar.bz2
+download http://pgfoundry.org/frs/download.php/1770/pg_top-$VERSION.tar.bz2
 build pg_top-$VERSION
