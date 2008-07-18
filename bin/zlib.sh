b0dfaff23a7180104b767524a78ddb8aca11c770
David Wheeler <david@justatheory.com>
2008-07-18 19:32:16 +0000

    Added zlib installation script.

---
+++ bin/zlib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export VERSION=1.2.3
+
+. `dirname $0`/functions.sh
+
+setup /usr/local/include/zlib.h "ZLIB_VERSION \"$VERSION\""
+download http://www.zlib.net/zlib-$VERSION.tar.gz
+build zlib-$VERSION
