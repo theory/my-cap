884b4af97bac428530326e014c111f8f4d78d2c8
David Wheeler <david@justatheory.com>
2008-09-07 22:37:16 +0000

    Added build script for wxwidgets.

---
+++ bin/wxwidgets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export VERSION=2.8.8
+
+. `dirname $0`/functions.sh
+
+setup
+download http://easynews.dl.sourceforge.net/sourceforge/wxwindows/wxWidgets-$VERSION.tar.gz
+cd wxWidgets-$VERSION
+mkdir osx-build
+cd osx-build
+../configure --disable-shared --enable-unicode --with-opengl
+make
+make install
