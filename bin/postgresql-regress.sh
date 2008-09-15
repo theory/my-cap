8568ca666db80799d090f4cd513a93cb1495b613
David Wheeler <david@justatheory.com>
2008-09-15 23:55:51 +0000

    Script to build older versions of PostgreSQL for purposes of regression testing.

---
+++ bin/postgresql-regress.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. `dirname $0`/functions.sh
+
+setup
+for VERSION in 8.2.9 8.1.13 8.0.17
+do
+    BASE=/usr/local/pgsql-$VERSION
+    if [ ! -e "$BASE" ]; then
+        download ftp://ftp10.us.postgresql.org/pub/postgresql/source/v$VERSION/postgresql-$VERSION.tar.bz2
+        rm -rf postgresql-$VERSION
+        tar jxf postgresql-$VERSION.tar.bz2 || exit $?
+        cd postgresql-$VERSION
+        ./configure --with-libs=/usr/local/lib --with-includes=/usr/local/include --prefix=$BASE 
+        make || exit $?
+        sudo make install || exit $?
+        sudo mkdir $BASE/data
+        sudo chown -R postgres:postgres $BASE/data
+        sudo -u postgres $BASE/bin/initdb --locale en_US.UTF-8 --encoding UNICODE -D $BASE/data
+        sudo mkdir $BASE/data/logs || exit $?
+        sudo chown -R postgres:postgres $BASE/data/logs
+        cd ..
+    fi
+done
