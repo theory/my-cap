a7973085d3ae3b87bb2da839d4dbc42c56da8dea
David Wheeler <david@justatheory.com>
2008-11-21 04:38:41 +0000

    Install some contrib modules, too.

--- bin/postgresql-devel.sh
+++ bin/postgresql-devel.sh
@@ -11,8 +11,28 @@ cvs up
 ./configure --with-libs=/usr/local/lib  --with-includes=/usr/local/include --prefix=$BASE --with-libxml || exit $?
 make || exit $?
 sudo make install || exit $?
+
+# Install contrib modules
+cd contrib
+svn export https://svn.kineticode.com/citext/trunk citext
+for dir in isn hstore uuid-ossp citext
+do
+    cd $dir
+    make || exit $?
+    make install || exit $?
+    make clean
+    cd ..
+done
+cd ..
+
 sudo mkdir $BASE/data || exit $?
 sudo chown -R postgres:postgres $BASE/data || exit $?
 sudo -u postgres $BASE/bin/initdb --locale en_US.UTF-8 --encoding utf-8 -D $BASE/data || exit $?
 sudo mkdir $BASE/data/logs || exit $?
 sudo chown -R postgres:postgres $BASE/data/logs || exit $?
+
+for file in isn hstore uuid-ossp citext
+do
+    $BASE/bin/psql -XU postgres -f $BASE/share/contrib/$file.sql template1
+    $BASE/bin/psql -XU postgres -f $BASE/share/contrib/$file.sql postgres
+done
