52fe6711bf228aa2b523c0b961e329486b90730a
David Wheeler <david@justatheory.com>
2009-02-03 19:06:47 +0000

    Install markdown-mode.

--- bin/elisp.sh
+++ bin/elisp.sh
@@ -2,21 +2,26 @@
 
 export MMMVERSION=0.4.8
 export PODVERSION=0.4
+export MDWNVERSION=0.6
 
 . `dirname $0`/functions.sh
 
-setup /usr/local/share/emacs/site-lisp/mmm-mode.el
+# Install mmm-mode.
+#setup /usr/local/share/emacs/site-lisp/mmm-mode.el
+setup
 download http://superb-west.dl.sourceforge.net/sourceforge/mmm-mode/mmm-mode-$MMMVERSION.tgz
 build mmm-mode-$MMMVERSION
 
+# pod-mode
 download http://cpan.org/authors/id/S/SC/SCHWIGON/pod-mode/pod-mode-$PODVERSION.tgz
 rm -rf pod-mode-$PODVERSION
 tar zxf pod-mode-$PODVERSION.tgz
 cd pod-mode-$PODVERSION
 emacs -batch -f batch-byte-compile pod-mode.el
-cp pod-mode.el* /usr/local/share/emacs/site-lisp
+cp pod-mode.el* 
 cd ..
 
+# ruby-mode
 if [ -d ruby-mode ]; then
     cd ruby-mode
     rm -rf *.elc
@@ -27,4 +32,14 @@ else
 fi
 emacs -batch -f batch-byte-compile *.el
 cp *.el* /usr/local/share/emacs/site-lisp
+cd ..
+
+# markdown-mode
+if [ !-d markdown-mode ]; then
+    mdkir markdown-mode
+fi
+cd markdown-mode
+download http://code.jblevins.org/markdown-mode/markdown-mode.el
+emacs -batch -f batch-byte-compile *.el
+cp markdown-mode.el* /usr/local/share/emacs/site-lisp
 cd ..
