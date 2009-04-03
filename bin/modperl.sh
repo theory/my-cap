760748bc8a49f88b706aec32f43cb5858e3a3e80
David Wheeler <david@justatheory.com>
2009-04-03 21:52:30 +0000

    No point in all that stuff, since tests simply will not run with DSO mod_perl.

--- bin/modperl.sh
+++ bin/modperl.sh
@@ -10,7 +10,7 @@ download http://people.apache.org/~gozer/mp1/mod_perl-$VERSION.tar.gz
 rm -rf mod_perl-$VERSION
 tar zxf mod_perl-$VERSION.tar.gz || exit $?
 cd mod_perl-$VERSION
-USER=dougm /usr/local/bin/perl Makefile.PL \
+perl Makefile.PL \
   USE_APXS=1 \
   WITH_APXS=/usr/local/apache/bin/apxs \
   USE_DSO=1 \
