631f23ac6decdc8c8a1adc3590dc83e7fd5337a3
David Wheeler <david@justatheory.com>
2009-04-03 18:07:24 +0000

    * Force use of the included APR.  * APR now prefers /dev/urandom, so I no longer have to convince it to do so.

--- bin/apache2.sh
+++ bin/apache2.sh
@@ -14,9 +14,6 @@ export CFLAGS="-DAP_UNSAFE_ERROR_LOG_UNESCAPED"
 # Debugging Apache.
 #export CFLAGS="-DAP_UNSAFE_ERROR_LOG_UNESCAPED -g"
 
-# Force APR to use /dev/urandom instead of /dev/random. Details on the issue
-# I found are here: http://www.andrewsavory.com/blog/archives/001408.html
-perl -i -pe 's{(/arandom\s+)/dev/random\s+}{$1}' srclib/apr/configure
 if [ "`ps ax | grep slapd | grep -v grep`" = '' ]; then
     LDAP=''
     SLDAP=''
@@ -28,6 +25,7 @@ fi
 ./configure \
  --prefix=/usr/local/apache2 \
  --with-mpm=prefork \
+ --with-included-apr \
  --enable-dav \
  --enable-dav-fs \
  --enable-log-config \
