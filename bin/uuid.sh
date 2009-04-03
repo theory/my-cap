6fd3e3d4df9549613c4c7ef3d33442d9b76ce060
David Wheeler <david@justatheory.com>
2009-04-03 18:03:02 +0000

    New version of uuid, plus a new home for its include file.

--- bin/uuid.sh
+++ bin/uuid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export VERSION=1.6.1
+export VERSION=1.6.2
 
 . `dirname $0`/functions.sh
 
@@ -9,7 +9,7 @@ download ftp://ftp.ossp.org/pkg/lib/uuid/uuid-$VERSION.tar.gz
 rm -rf uuid-$VERSION
 tar zxf uuid-$VERSION.tar.gz
 cd uuid-$VERSION
-./configure --prefix=/usr/local --with-perl=/usr/local/bin/perl
+./configure --prefix=/usr/local --with-perl=/usr/local/bin/perl --includedir=/usr/local/include/ossp-uuid 
 make
 make check
 make install
