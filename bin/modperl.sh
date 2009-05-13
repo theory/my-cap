5e51f933ab821b3359b5d0b0b26f2b7e2a478352
David Wheeler <david@justatheory.com>
2009-05-13 03:33:45 +0000

    mod_perl 1.31 final finally released. W00t!

--- bin/modperl.sh
+++ bin/modperl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export VERSION=1.31-rc8
+export VERSION=1.31
 
 . `dirname $0`/functions.sh
 
 setup /usr/local/lib/perl5/site_perl/5.10.0/darwin-2level/mod_perl.pm "VERSION = \"$VERSION\";"
-download http://people.apache.org/~gozer/mp1/mod_perl-$VERSION.tar.gz
-#download http://perl.apache.org/dist/mod_perl-$VERSION.tar.gz
+#download http://people.apache.org/~gozer/mp1/mod_perl-$VERSION.tar.gz
+download http://perl.apache.org/dist/mod_perl-$VERSION.tar.gz
 rm -rf mod_perl-$VERSION
 tar zxf mod_perl-$VERSION.tar.gz || exit $?
 cd mod_perl-$VERSION
