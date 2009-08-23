7f224c5679bb188dff3ae9faca9469f382bb4c26
David Wheeler <david@justatheory.com>
2009-08-23 19:09:29 +0000

    New version of Perl.

--- bin/perl.sh
+++ bin/perl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export VERSION=5.10.0
+export VERSION=5.10.1
 
 . `dirname $0`/functions.sh
 
 setup /usr/local/bin/perl$VERSION
-download http://www.cpan.org/src/perl-$VERSION.tar.gz
+download http://www.cpan.org/src/perl-$VERSION.tar.bz2
 rm -rf perl-$VERSION
-tar zxf perl-$VERSION.tar.gz || exit $?
+tar jxf perl-$VERSION.tar.bz2 || exit $?
 cd perl-$VERSION
 # Debugging Perl:
 #sh Configure -des -Duseshrplib -Dprefix=/usr/local/perl-5.10.0.d -Doptimize='-g' || exit $?
