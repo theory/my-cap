3d1cc43fe5c55ba3cfd78c0a2e8fd9b346310a71
David Wheeler <david@justatheory.com>
2009-09-24 00:18:47 +0000

    Pg devel update.

--- bin/postgresql-devel.sh
+++ bin/postgresql-devel.sh
@@ -5,11 +5,12 @@ export PERL=/usr/local/bin/perl
 export BASE=/usr/local/pgsql-$VERSION
 
 cd ~/dev/postgresql
-if [-f GNUmakefile]; then
+if [ -f GNUmakefile ]; then
     make maintainer-clean
 fi
 git checkout master
 git pull
+rm -rf $BASE
 # For debugging: --enable-cassert --enable-debug
 ./configure --with-libs=/usr/local/lib  --with-includes=/usr/local/include --prefix=$BASE --with-libxml  --with-ossp-uuid || exit $?
 make -j3 || exit $?
@@ -27,10 +28,13 @@ do
     cd ..
 done
 cd ..
-exit
+
 sudo mkdir $BASE/data || exit $?
 sudo chown -R postgres:postgres $BASE/data || exit $?
 sudo -u postgres $BASE/bin/initdb --locale en_US.UTF-8 --encoding utf-8 -D $BASE/data || exit $?
+sudo mkdir $BASE/data/logs
+sudo chown -R postgres:postgres $BASE/data/logs
+
 # sudo mkdir $BASE/data/pg_log || exit $?
 # sudo chown -R postgres:postgres $BASE/data/pg_log || exit $?
 
