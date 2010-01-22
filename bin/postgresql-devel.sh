d5f32e70d69e2d07e51e8af55e9eb039dbc95098
David Wheeler <david@justatheory.com>
2010-01-22 17:51:33 +0000

    Always build with --enable-cassert and --enable-debug.

--- bin/postgresql-devel.sh
+++ bin/postgresql-devel.sh
@@ -11,8 +11,7 @@ fi
 git checkout master
 git pull
 rm -rf $BASE
-# For debugging: --enable-cassert --enable-debug
-./configure --with-libs=/usr/local/lib  --with-includes=/usr/local/include --prefix=$BASE --with-libxml  --with-ossp-uuid --with-perl PERL=$PERL || exit $?
+./configure --with-libs=/usr/local/lib  --with-includes=/usr/local/include --prefix=$BASE --with-libxml  --with-ossp-uuid --with-perl PERL=$PERL --enable-cassert --enable-debug || exit $?
 make -j3 || exit $?
 sudo make install || exit $?
 
