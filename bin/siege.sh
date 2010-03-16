5623f9adb0dcf269b5c5f3a4d8e6f7a6a7fb5e35
David Wheeler <david@justatheory.com>
2010-03-16 20:32:12 +0000

    Install sproxy-version; new versions of PostgreSQL.

--- bin/siege.sh
+++ bin/siege.sh
@@ -9,3 +9,7 @@ download ftp://ftp.joedog.org/pub/siege/siege-$VERSION.tar.gz
 mkdir -p /usr/local/etc
 build siege-$VERSION
 #make check
+
+export VERSION=1.01
+download ftp://sid.joedog.org/pub/sproxy/sproxy-$VERSION.tar.gz
+build sproxy-$VERSION
