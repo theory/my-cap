6f7ae9bbee51a6e9eab44f26c0984595f151ea1e
David Wheeler <david@justatheory.com>
2010-04-06 20:42:25 +0000

    Compile without debugging.

--- bin/postgresql-devel.sh
+++ bin/postgresql-devel.sh
@@ -11,7 +11,8 @@ fi
 git checkout master
 git pull
 rm -rf $BASE
-./configure --with-libs=/usr/local/lib  --with-includes=/usr/local/include --prefix=$BASE --with-libxml  --with-ossp-uuid --with-perl PERL=$PERL --enable-cassert --enable-debug || exit $?
+# Add  --enable-cassert --enable-debug for debugging.
+./configure --with-libs=/usr/local/lib  --with-includes=/usr/local/include --prefix=$BASE --with-libxml  --with-ossp-uuid --with-perl PERL=$PERL || exit $?
 make -j3 || exit $?
 sudo make install || exit $?
 
