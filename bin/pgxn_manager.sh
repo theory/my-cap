ae2727ac6ec20e43aafa4d5754d018b150eb4ec3
David Wheeler <david@justatheory.com>
2010-10-09 03:16:42 +0000

    Add PGXN Manager build and configuration.

---
+++ bin/pgxn_manager.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+export BASE=/var/www/manager.pgxn.org
+export PERL=/usr/local/bin/perl
+export ROOT=/var/www/master.pgxn.org
+export PGXNUSER=pgxn
+
+# Check for user ID.
+id -u $PGXNUSER >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    useradd $PGXNUSER -d /nonexistent
+fi
+
+# Check for mirror root.
+if [ ! -d $ROOT ]; then
+    mkdir -p $ROOT
+    chown -R $PGXNUSER:$PGXNUSER $ROOT
+fi
+
+# Pull or clone the repository.
+if [ -d $BASE ]; then
+    cd $BASE
+    git pull origin master
+else
+    cd `dirname $BASE`
+    git clone git://github.com/theory/pgxn-manager.git `basename $BASE`
+fi
+
+# Build it!
+$PERL Build.PL --db_super_user postgres \
+               --db_client /usr/local/pgsql/bin/psql \
+               --context prod
+./Build
+./Build db
