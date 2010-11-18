f0e93df83e714751322b4547605c6afd1a2b8f89
David Wheeler <david@justatheory.com>
2010-11-18 05:37:17 +0000

    Add script to build Mercurial.

---
+++ bin/mercurial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export VERSION=1.7.1
+
+. `dirname $0`/functions.sh
+
+setup /usr/local/lib/python2.6/site-packages/mercurial/__version__.py $VERSION
+download http://mercurial.selenic.com/release/mercurial-$VERSION.tar.gz
+tar zxf mercurial-$VERSION.tar.gz
+cd mercurial-$VERSION
+make build
+make install-bin
+cd ..
