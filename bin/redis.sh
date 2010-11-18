dc760a2b35b992b1a5ed0a22a284321982b18b05
David Wheeler <david@justatheory.com>
2010-11-18 23:02:17 +0000

    redis 1.2.x

--- bin/redis.sh
+++ bin/redis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export VERSION=2.0.4
+#export VERSION=2.0.4
+export VERSION=1.2.6
 
 . `dirname $0`/functions.sh
 
@@ -9,8 +10,16 @@ download http://redis.googlecode.com/files/redis-$VERSION.tar.gz
 tar zxf redis-$VERSION.tar.gz
 cd redis-$VERSION
 make
-make test
-make install
+# These two work under 2.0:
+# make test
+# make install
+
+# Just use these for 1.x:
+cp redis-server /usr/local/bin/
+cp redis-benchmark /usr/local/bin/
+cp redis-cli /usr/local/bin/
+cp redis-stat /usr/local/bin/
+
 cd ..
 
 
