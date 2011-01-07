2cc280be5e33c0d4edeeb14952e9a724aa250929
David Wheeler <david@justatheory.com>
2011-01-07 07:14:44 +0000

    New version of libxml2.

--- bin/libxml2.sh
+++ bin/libxml2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export VERSION=2.7.7
+export VERSION=2.7.8
 
 . `dirname $0`/functions.sh
 
-setup /usr/local/include/libxml2/libxml/parser.h
+setup /usr/local/include/libxml2/libxml/parser.h "echo $VERSION"
 download ftp://xmlsoft.org/libxml2/libxml2-$VERSION.tar.gz
 build libxml2-$VERSION
