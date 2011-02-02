22c6363948e66fa8f471721374267ff45769f1a3
David Wheeler <david@justatheory.com>
2011-02-02 03:48:05 +0000

    New version of Discount.

--- bin/markdown.sh
+++ bin/markdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DVERSION=2.0.1
+export DVERSION=2.0.5
 export MVERSION=1.0.1
 . `dirname $0`/functions.sh
 
@@ -10,8 +10,8 @@ unzip Markdown_$MVERSION.zip || exit $?
 cp Markdown_$MVERSION/Markdown.pl /usr/local/bin
 chmod +x /usr/local/bin/Markdown.pl
 
-download http://www.pell.portland.or.us/%7Eorc/Code/markdown/discount-$DVERSION.tar.gz
-tar zxf discount-$DVERSION.tar.gz || exit $?
+download http://www.pell.portland.or.us/%7Eorc/Code/markdown/discount-$DVERSION.tar.bz2
+tar jxf discount-$DVERSION.tar.bz2 || exit $?
 cd discount-$DVERSION || exit $?
 ./configure.sh || exit $?
 make || exit $?
