757a3f8d1311e93a341194a389f14eb1794c8e28
David Wheeler <david@justatheory.com>
2011-02-27 01:20:49 +0000

    Update discount build for latest version.
    Thanks to the new `--with-id-anchor` option, I no longer need the old version.
    Phew! This version isn't released yet, but soon will be, I expect.

--- bin/markdown.sh
+++ bin/markdown.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export DVERSION=2.0.5
-export ODVERSION=1.6.7
+export DVERSION=2.0.7
 export MVERSION=1.0.1
 . `dirname $0`/functions.sh
 
@@ -14,18 +13,8 @@ chmod +x /usr/local/bin/Markdown.pl
 download http://www.pell.portland.or.us/%7Eorc/Code/markdown/discount-$DVERSION.tar.bz2
 tar jxf discount-$DVERSION.tar.bz2 || exit $?
 cd discount-$DVERSION || exit $?
-./configure.sh || exit $?
+./configure.sh --with-id-anchor --enable-all-features || exit $?
 make || exit $?
 make install || exit $?
-cd ..
 
-download http://www.pell.portland.or.us/%7Eorc/Code/markdown/discount-$ODVERSION.tar.bz2
-tar jxf discount-$ODVERSION.tar.bz2 || exit $?
-cd discount-$ODVERSION || exit $?
-./configure.sh --prefix /usr/local/discount-$ODVERSION|| exit $?
-make || exit $?
-mkdir -p /usr/local/discount-$ODVERSION/bin
-mkdir -p /usr/local/discount-$ODVERSION/lib
-mkdir -p /usr/local/discount-$ODVERSION/include
-make install || exit $?
 cd ..
