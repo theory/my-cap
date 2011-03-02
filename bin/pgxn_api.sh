824647914218e1a5968678d6344ce8c0882bc365
David Wheeler <david@justatheory.com>
2011-03-02 23:54:42 +0000

    Add PGXN API configuration.

---
+++ bin/pgxn_api.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export BASE=/var/www/api.pgxn.org
+export PERL=/usr/local/bin/perl
+export VERSION=0.1.0
+
+# Pull or clone the repository.
+if [ -d $BASE ]; then
+    cd $BASE
+    git fetch origin
+    git pull origin tag v$VERSION
+else
+    cd `dirname $BASE`
+    git clone git://github.com/theory/pgxn-api.git `basename $BASE`
+fi
+
+# Build it!
+$PERL Build.PL
+./Build
+
