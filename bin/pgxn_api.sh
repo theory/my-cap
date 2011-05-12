ce6d3ab6b8bf407de79541e237b516c855fc150b
David Wheeler <david@justatheory.com>
2011-05-12 18:14:08 +0000

    New versions of everything.

--- bin/pgxn_api.sh
+++ bin/pgxn_api.sh
@@ -2,9 +2,9 @@
 
 export BASE=/var/www/api.pgxn.org
 export PERL=/usr/local/bin/perl
-export VERSION=0.14.0
-export SVERSION=0.9.2
-export WVERSION=0.12.0
+export VERSION=0.14.1
+export SVERSION=0.9.3
+export WVERSION=0.12.1
 
 cd github/pgxn-api-searcher
 git fetch origin
