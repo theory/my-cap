e62133c17fe84f1aa12b716429ee06d2c2dcef3a
David E. Wheeler <david@justatheory.com>
2011-08-05 10:56:29 -0700

    libpng fixed, no more need for workarounds.

--- bin/libpng.sh
+++ bin/libpng.sh
@@ -6,10 +6,4 @@ export VERSION=1.5.4
 
 setup /usr/local/include/png.h
 download ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-$VERSION.tar.gz
-rm -rf libpng-$VERSION
-tar zxf libpng-$VERSION.tar.gz
-cd libpng-$VERSION
-cp scripts/makefile.darwin makefile
-perl -i -pe 's/PNGVER\s*=\s*\$\(PNGMAJ\)\./PNGVER = /' makefile
-make
-make install
+build libpng-$VERSION
