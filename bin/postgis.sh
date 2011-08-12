9e04b5e5a2eb85102481ab4c5c50724f6992467e
David E. Wheeler <david@justatheory.com>
2011-08-12 12:15:38 -0700

    New version of PostGIS.

--- bin/postgis.sh
+++ bin/postgis.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-export VERSION=1.5.1
+export VERSION=1.5.3
 
 . `dirname $0`/functions.sh
 
 setup /usr/local/pgsql/share/contrib/postgis-1.5/postgis.sql
-# download http://www.postgis.org/download/postgis-$VERSION.tar.gz
-# tar zxf postgis-$VERSION.tar.tz
-# cd postgis-$VERSION
-cd /Users/david/dev/sql/postgis/1.5
+download http://www.postgis.org/download/postgis-$VERSION.tar.gz
+tar zxf postgis-$VERSION.tar.tz
+cd postgis-$VERSION
 ./configure --with-pgconfig=/usr/local/pgsql/bin/pg_config
 make
 make install
