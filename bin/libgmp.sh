e2998642b0ad0b8cfb6e05b710122a847be57c06
David E. Wheeler <david@justatheory.com>
2011-08-30 10:18:21 -0700

    Build gmp manually.

--- bin/libgmp.sh
+++ bin/libgmp.sh
@@ -6,4 +6,13 @@ export VERSION=5.0.2
 
 setup /usr/local/include/gmp.h
 download ftp://ftp.gmplib.org/pub/gmp-$VERSION/gmp-$VERSION.tar.bz2
-build gmp-$VERSION
+#build gmp-$VERSION
+
+# Build manually for now, as file needs removing and `make check` is important
+tar jxf gmp-$VERSION.tar.bz2
+cd gmp-$VERSION
+rm mpn/x86_64/mod_34lsub1.asm
+make || exit $?
+make check || exit $?
+make install || exit $?
+
