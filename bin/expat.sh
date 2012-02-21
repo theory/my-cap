c6b8347528075b30fe4e4dc4adf7981e83cc95f4
David E. Wheeler <david@justatheory.com>
2012-02-21 14:04:24 -0800

    New download URL.

--- bin/expat.sh
+++ bin/expat.sh
@@ -5,6 +5,6 @@ export VERSION=2.0.1
 . `dirname $0`/functions.sh
 
 setup /usr/local/include/expat.h
-download http://softlayer.dl.sourceforge.net/project/expat/expat/$VERSION/expat-$VERSION.tar.gz
+download http://downloads.sourceforge.net/project/expat/expat/$VERSION/expat-$VERSION.tar.gz
 build expat-$VERSION
 #make check
