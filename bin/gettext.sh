6df1aa52ca67ab751308ea4d8214ac83dd8c6ab2
David E. Wheeler <david@justatheory.com>
2012-06-05 13:42:13 -0700

    Install (and fix) gettext.

--- bin/gettext.sh
+++ bin/gettext.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VERSION=0.17
+export VERSION=0.18.1.1
 
 . `dirname $0`/functions.sh
 
@@ -9,4 +9,12 @@ if [ "`/usr/local/bin/gettext --version | head -1 | awk '{print $4}'`" = $VERSIO
 fi
 setup
 download ftp://ftp.gnu.org/gnu/gettext/gettext-$VERSION.tar.gz
-build gettext-$VERSION
+# XXX Ugh, have to patch it, so have to build it manually ourselves.
+#build gettext-$VERSION
+tar zxf gettext-$VERSION.tar.gz
+cd gettext-$VERSION
+curl 'https://trac.macports.org/export/79617/trunk/dports/devel/gettext/files/stpncpy.patch' | patch -p0
+./configure
+make
+make install
+
