98eccbbdcc1323ae5c5d03638cc5cd1329bbe649
David E. Wheeler <david@justatheory.com>
2013-07-26 15:00:38 +0200

    Check for v8 include before building.

--- bin/v8.sh
+++ bin/v8.sh
@@ -4,7 +4,7 @@ export VERSION=3.20.9
 
 . `dirname $0`/functions.sh
 
-setup
+setup /usr/local/include/v8.h
 download https://github.com/v8/v8/archive/$VERSION.tar.gz
 tar zxf $VERSION.tar.gz
 mv $VERSION v8-$VERSION
@@ -12,6 +12,7 @@ cd v8-$VERSION
 
 make dependencies
 make -j4 native library=shared snapshot=on console=readline
+cp include/*.h /usr/local/include 
 cd out/native
 cp lib* /usr/local/lib/
 cp d8 /usr/local/bin/
