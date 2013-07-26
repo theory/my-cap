7ecfe9127ae7b5615c8e2df56943b0f802045ceb
David E. Wheeler <david@justatheory.com>
2013-07-26 14:50:24 +0200

    Add v8.

---
+++ bin/v8.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export VERSION=3.20.9
+
+. `dirname $0`/functions.sh
+
+setup
+download https://github.com/v8/v8/archive/$VERSION.tar.gz
+tar zxf $VERSION.tar.gz
+mv $VERSION v8-$VERSION
+cd v8-$VERSION
+
+make dependencies
+make -j4 native library=shared snapshot=on console=readline
+cd out/native
+cp lib* /usr/local/lib/
+cp d8 /usr/local/bin/
+cp lineprocessor /usr/local/bin/
+cp preparser /usr/local/bin/
+cp process /usr/local/bin/
+cp shell /usr/local/bin/v8
+cp mksnapshot.* /usr/local/bin/
