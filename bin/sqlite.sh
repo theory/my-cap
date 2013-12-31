db15d2916311b2e2d891855ee2584b43dfcf499f
David E. Wheeler <david@justatheory.com>
2013-12-31 10:26:06 -0800

    Update SQLite.

--- bin/sqlite.sh
+++ bin/sqlite.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-export VERSION=3071502
+export VERSION=3080200
+export YEAR=2013
 
 . `dirname $0`/functions.sh
 
 setup /usr/local/include/sqlite.h
-download http://sqlite.org/sqlite-autoconf-$VERSION.tar.gz
+download https://sqlite.org/$YEAR/sqlite-autoconf-$VERSION.tar.gz
 build sqlite-autoconf-$VERSION
 
