248a66246a78709d904cc4b2930ec73446a73600
David E. Wheeler <david@justatheory.com>
2014-06-25 15:26:27 -0700

    Update Postgres build.

--- bin/postgresql-devel.sh
+++ bin/postgresql-devel.sh
@@ -2,7 +2,7 @@
 
 export VERSION=devel
 export PERL=/usr/local/bin/perl
-export BASE=/usr/local/pgsql-$VERSION
+export BASE=/usr/local/pgsql
 export CPPFLAGS=-D_XOPEN_SOURCE
 
 cd ~/dev/postgresql/postgresql
@@ -10,10 +10,12 @@ if [ -f GNUmakefile ]; then
     make maintainer-clean
 fi
 # git checkout master
-git pull
+#git pull
 sudo rm -rf $BASE
 # Add  --enable-cassert --enable-debug for debugging.
-./configure --with-libs=/usr/local/lib  --with-includes=/usr/local/include --prefix=$BASE --with-libxml  --with-ossp-uuid --with-perl PERL=$PERL || exit $?
+./configure --with-perl PERL=$PERL --with-openssl --with-pam \
+    --with-libxml --with-uuid=e2fs --with-libs=/usr/local/lib \
+    --with-includes=/usr/local/include || exit $?
 make -j3 || exit $?
 sudo make install || exit $?
 
