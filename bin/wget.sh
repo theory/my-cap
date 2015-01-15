410254d2d04b03e5da3d686e28c86126909d63ec
David E. Wheeler <david@justatheory.com>
2015-01-14 17:16:54 -0800

    Add wget.

---
+++ bin/wget.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export VERSION=1.16.1
+
+. `dirname $0`/functions.sh
+
+setup
+download http://ftp.gnu.org/gnu/wget/wget-$VERSION.tar.gz
+rm -rf wget-$VERSION
+tar zxf wget-$VERSION.tar.gz
+cd wget-$VERSION
+./configure --prefix=/usr/local --with-ssl=openssl
+make
+make install
+
+#make check
